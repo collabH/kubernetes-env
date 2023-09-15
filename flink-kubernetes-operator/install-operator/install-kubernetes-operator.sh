b3531f94a744fdaafcea26bfff40b7009adc66d4
huangshimin <h1261109615@qq.com>
2023-09-15 19:00:58 +0800

    Update install-kubernetes-operator.sh

--- flink-kubernetes-operator/install-operator/install-kubernetes-operator.sh
+++ flink-kubernetes-operator/install-operator/install-kubernetes-operator.sh
@@ -8,7 +8,7 @@
 # install cert-manager
 kubectl create -f https://github.com/jetstack/cert-manager/releases/download/v1.8.2/cert-manager.yaml
 # add flink operator repo helm chart
-helm repo add flink-operator-repo https://downloads.apache.org/flink/flink-kubernetes-operator-1.1.0
+helm repo add flink-operator-repo160 https://downloads.apache.org/flink/flink-kubernetes-operator-1.6.0/
 
 # install flink operator use default helm chart conf
-helm install flink-kubernetes-operator flink-operator-repo/flink-kubernetes-operator -n flink-operator
+helm install flink-kubernetes-operator flink-operator-repo160/flink-kubernetes-operator
